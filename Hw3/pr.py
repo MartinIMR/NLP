3d855d015e944839c2d1084f34c92eb0ce502e3c
MartinIMR <martin.mimr@gmail.com>
2019-09-09 16:23:03 -0500

    Changes to current version

--- Hw3/pr.py
+++ Hw3/pr.py
@@ -21,11 +21,49 @@ def tag_sentences(sentences):
     tokens = nltk.word_tokenize(sent)
     s_tagged = tagger.tag(tokens)
     tagged = tagged + s_tagged
-  return tagged 
+  return tagged
+
+def lemmatization(pairs):
+  from pickle import load 
+  input = open("lemmas.pkl","rb")
+  lemmas = load(input)
+  input.close()
+  lemma_dict = []
+  for pair in pairs:
+    key = pair[0]+" "+pair[1][0] #Word and category
+    if key in lemmas: 
+     lemma = lemmas[key]
+     lemma_dict.append(lemma)
+  return lemma_dict
+
+def get_context_list(word,tagged_words):
+  window = 8
+  bound = len(tagged_words)
+  moves = (window//2)
+  context_list = []
+  for pair in tagged_words:
+    if word == pair[0]:
+     index = tagged_words.index(pair)
+     min = index - moves
+     max = index + moves + 1
+     if min < 0:
+      min = 0
+     if max > bound:
+      max = bound
+     ady_list = []
+     print("The word index is:",index)
+     for i in range(min,max): #Ocurrences
+       if i != index:
+         print("The word in position:",i,"is added")
+         ady_list.append(tagged_words[i]) 
+     context_list = context_list + ady_list 
+  return context_list
 
 if __name__=='__main__':
   file_name = "e960401_mod.htm"
   r_sentences = tokenize(file_name) #Getting the text string 
   tagged_words = tag_sentences(r_sentences) #Tag sentences
-  print("Tagged list has:",len(tagged_words)," elements")
-  print(tagged_words[:200])
+  lemma_dict = lemmatization(tagged_words)
+  vocabulary = set(lemma_dict)
+  lista = get_context_list("caminar",tagged_words)
+  print(lista)
