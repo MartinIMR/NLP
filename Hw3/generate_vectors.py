acd189e85663876f939cbc2f2f89eccf3e549c63
Martín Rogel <martin.mimr@gmail.com>
2019-09-20 00:35:14 -0500

    Changes not proved yet

--- Hw3/generate_vectors.py
+++ Hw3/generate_vectors.py
@@ -38,6 +38,49 @@ def count_freq(vocabulary,v_dict,c_dict):
    index = vocabulary.index(pair)
    vector[index] = vector[index] + 1
 
+def get_avg(context_dict):
+ key_list = context_dict.keys()
+ print("The voc size is:")
+ size = len(key_list)
+ sum = 0
+ for key in key_list:
+  sum = sum + len(context_dict[key])
+ print("The total sum is:",sum)
+ average = sum/size
+ print("The average is:",average)
+ return average
+
+def count_context(word,context):
+ count = 0
+ for pair in context:
+   if pair[0] == word:
+    count = count + 1
+ return count
+
+def calc_bm25(word,context):
+ """ This function arguments are the word and the 
+     list of words' context """
+ k = 1.5
+ b = 0.75
+ count = count_context(word,context)
+ size = len(context)
+ average = get_avg(context_dict)
+ numerator = (k+1)*count
+ denominator = count + k*( (1-b) + b*(average/numerator))
+ bm25 = numerator / denominator
+ return bm25
+
+def bm25_vector(context_dict,context):
+ key_list = context_dict.keys()
+ dimension = len(key_list)
+ vector = np.zeros(dimension, dtype=float)
+ sum = 0
+ for i in range(dimension): #For each word calculate bm25 with that context
+  vector[i] = calc_bm25(key_list[i][0],context) 
+  sum = sum + vector[i]
+ vector = vector / sum
+ return vector 
+
 def eowc_freq(vocabulary,v_dict,c_dict):
  for i in range(len(vocabulary)):
   key = vocabulary[i]
@@ -76,6 +119,11 @@ if __name__=='__main__':
    vocabulary = load_data("vocabulary.pkl")
    vectors_dict = create_vdict(vocabulary) #Create vector's dictionary
    context_dict = get_cdict(vocabulary,lemma_list) #Get ocurrences's dictionary
+   word = "crecer" 
+   entry = get_entry(word,vocabulary)
+   context = context_dict[entry]
+   bm25wv = bm25_vector(context_dict,context)
+
    """ Raw count frequency """
    #count_freq(vocabulary,vectors_dict,context_dict) #Raw frequency 
    #save_data(vectors_dict,"count_vectors.pkl")
